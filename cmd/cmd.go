46e2ebd83a47522420ee1f849ce0fbaac35803d0
Gabe Cook <gabe565@gmail.com>
2023-04-10 00:29:42 -0500

    :sparkles: (config): Add env support

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/gabe565/ascii-movie/cmd/play"
 	"github.com/gabe565/ascii-movie/cmd/serve"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
+	"os"
+	"strings"
 )
 
 func NewCommand() *cobra.Command {
@@ -11,6 +15,7 @@ func NewCommand() *cobra.Command {
 		Use:   "ascii-movie",
 		Short: "Command line ASCII movie player.",
 
+		PersistentPreRunE: FlagEnvs,
 		DisableAutoGenTag: true,
 	}
 	cmd.AddCommand(
@@ -18,4 +23,21 @@ func NewCommand() *cobra.Command {
 		serve.NewCommand(),
 	)
 	return cmd
+}
+
+const EnvPrefix = "ASCII_MOVIE_"
+
+func FlagEnvs(cmd *cobra.Command, args []string) error {
+	var errs []error
+	cmd.Flags().VisitAll(func(f *flag.Flag) {
+		optName := strings.ToUpper(f.Name)
+		optName = strings.ReplaceAll(optName, "-", "_")
+		varName := EnvPrefix + optName
+		if val, ok := os.LookupEnv(varName); !f.Changed && ok {
+			if err := f.Value.Set(val); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	})
+	return errors.Join(errs...)
 }
