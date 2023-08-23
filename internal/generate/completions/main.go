bbf061c530388ea93a3b4d9dceb018682f5c9825
Gabe Cook <gabe565@gmail.com>
2023-08-22 22:21:42 -0500

    feat: Add completion generator

---
+++ internal/generate/completions/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/gabe565/ascii-movie/cmd"
+)
+
+var Shells = []string{"bash", "zsh", "fish"}
+
+func main() {
+	if err := os.RemoveAll("completions"); err != nil {
+		panic(err)
+	}
+
+	if err := os.MkdirAll("completions", 0o777); err != nil {
+		panic(err)
+	}
+
+	rootCmd := cmd.NewCommand("latest", "")
+	name := rootCmd.Name()
+	var buf bytes.Buffer
+	rootCmd.SetOut(&buf)
+
+	for _, shell := range Shells {
+		rootCmd.SetArgs([]string{"completion", shell})
+		if err := rootCmd.Execute(); err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(filepath.Join("completions", name+"."+shell))
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := io.Copy(f, &buf); err != nil {
+			panic(err)
+		}
+
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
